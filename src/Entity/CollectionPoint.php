852041addc646553d833e2ebea0e3327929d1e13
Olivier <olivier.arnac@laposte.net>
2020-06-10 14:20:23 +0200

    Ajout de l'entity CollectionPoint

---
+++ src/Entity/CollectionPoint.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CollectionPointRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CollectionPointRepository::class)
+ */
+class CollectionPoint
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $streetNumber;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $streetName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $zipCode;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $coordinateX;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $coordinateY;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $openingTime;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $review;
+
+    /**
+     * @ORM\Column(type="string", length=45, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $website;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStreetNumber(): ?string
+    {
+        return $this->streetNumber;
+    }
+
+    public function setStreetNumber(string $streetNumber): self
+    {
+        $this->streetNumber = $streetNumber;
+
+        return $this;
+    }
+
+    public function getStreetName(): ?string
+    {
+        return $this->streetName;
+    }
+
+    public function setStreetName(string $streetName): self
+    {
+        $this->streetName = $streetName;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode(string $zipCode): self
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    public function getCoordinateX(): ?float
+    {
+        return $this->coordinateX;
+    }
+
+    public function setCoordinateX(?float $coordinateX): self
+    {
+        $this->coordinateX = $coordinateX;
+
+        return $this;
+    }
+
+    public function getCoordinateY(): ?float
+    {
+        return $this->coordinateY;
+    }
+
+    public function setCoordinateY(?float $coordinateY): self
+    {
+        $this->coordinateY = $coordinateY;
+
+        return $this;
+    }
+
+    public function getOpeningTime(): ?string
+    {
+        return $this->openingTime;
+    }
+
+    public function setOpeningTime(string $openingTime): self
+    {
+        $this->openingTime = $openingTime;
+
+        return $this;
+    }
+
+    public function getReview(): ?string
+    {
+        return $this->review;
+    }
+
+    public function setReview(?string $review): self
+    {
+        $this->review = $review;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getWebsite(): ?string
+    {
+        return $this->website;
+    }
+
+    public function setWebsite(?string $website): self
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
