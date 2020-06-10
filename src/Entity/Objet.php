4bf72841b814f91295b7f337b9f0153062b5d2ea
Olivier <olivier.arnac@laposte.net>
2020-06-10 14:11:25 +0200

    Ajout de l'entity objet

---
+++ src/Entity/Objet.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ObjetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ObjetRepository::class)
+ */
+class Objet
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $avoidProduction;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAvoidProduction(): ?string
+    {
+        return $this->avoidProduction;
+    }
+
+    public function setAvoidProduction(?string $avoidProduction): self
+    {
+        $this->avoidProduction = $avoidProduction;
+
+        return $this;
+    }
+}
