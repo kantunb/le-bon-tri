68d65e430ab1f30a2496aa0097d0071e13b3a422
Olivier <olivier.arnac@laposte.net>
2020-06-11 12:48:51 +0200

    Création des controllers

---
+++ src/Controller/ResultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ResultController extends AbstractController
+{
+    /**
+     * @Route("/result", name="result")
+     */
+    public function index()
+    {
+        return $this->render('result/index.html.twig', [
+            'controller_name' => 'ResultController',
+        ]);
+    }
+}
