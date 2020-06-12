583284f8a703b409742a3c484848230eb40e586b
Pénélope PRINGALLE <penelopepringalle@gmail.com>
2020-06-12 17:32:24 +0200

    gestion form de recherche

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,10 @@
+var options = {
+	url: function(name) {
+		//return "api/objetJson.php?phrase=" + phrase + "&format=json";
+		return "ajaxsearch?name=" + name;
+	},
+
+	getValue: "name"
+};
+
+$("#provider-remote").easyAutocomplete(options);
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,21 +3,47 @@
 namespace App\Controller;
 
 use App\Entity\Objet;
+use App\Form\AppType;
+use App\Repository\ObjetRepository;
+use Symfony\Bridge\Twig\AppVariable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class AppController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
      */
-    public function index()
-    {
+    public function index(ObjetRepository $objetRepository)
+    {   
+      // $objet = $this->getDoctrine()->getRepository(Objet::class)->findAll();
+      
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+            'objets' => $objetRepository->findAll()
+            //json_encode($objet)
         ]);
     }
+
+/**
+ *@Route("/ajaxsearch", name="app_json")
+ */
+public function jsonOfAllObjects(Request $request){
+   $search = $request->query->get('name');
+
+    $objets = $this->getDoctrine()->getRepository(Objet::class)->findBy($search);
+ //   dd($objets);
+        $objetsList = [];
+    
+foreach($objets as $objet){
+        $objet =['name' => $objet->getName()];
+        $objetsList[] = $objet;
+    }
+    return new JsonResponse($objetsList);
+}
 
 /**
 * @Route("/search", name="app_search", methods={"GET"})
@@ -46,4 +72,6 @@ public function search(Request $request) {
     // Sinon, on redirige en page d'accueil
     return $this->redirectToRoute("app_index");
 }
+
+
 }
