9a447a3d7981367e52c424d33873cd4d0c0978ef
Pénélope PRINGALLE <penelopepringalle@gmail.com>
2020-06-16 10:13:15 +0200

    regex

--- src/Entity/CollectionPointType.php
+++ src/Entity/CollectionPointType.php
@@ -6,6 +6,7 @@ use App\Repository\CollectionPointTypeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CollectionPointTypeRepository::class)
@@ -21,6 +22,12 @@ class CollectionPointType
 
     /**
      * @ORM\Column(type="string", length=45)
+     *    @Assert\Regex(
+     *     pattern     = "/^[a-zA-Z0-9()_, -]{3,255}+$/i",
+     *     htmlPattern = "^[a-zA-Z0-9()_, -]{3,255}+$",
+     *     match=true,
+     *     message="Le type du point de collecte peut contenir des lettres majusccules, minuscules, des chiffres, des parenthèses, - , _ , des virgules ou des 
+     *      espaces"
      */
     private $type;
 
